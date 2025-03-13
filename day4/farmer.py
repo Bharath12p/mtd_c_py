d1dfdfaf138a7b9bb471b0965494910001e5e6b8
bharath12p <bharath2004bp@gmail.com>
2025-03-13 15:41:52 +0530

    new files

---
+++ day4/farmer.py
@@ -0,0 +1,8 @@
+total_land = 80
+tomato_sales_1 = total_land/5.0 * 0.3 * 7
+tomato_sales_2 = total_land/5.0 * 0.7 * 7
+
+total_tomato_sales = tomato_sales_1 + tomato_sales_2
+potato_sales = total_land/5.0 *10 * 1000* 20
+cabbage_sales = total_land
+
