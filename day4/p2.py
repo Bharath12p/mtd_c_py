bdfdafad7f44a868ee0b253334f3563113224895
bharath12p <bharath2004bp@gmail.com>
2025-03-13 12:12:07 +0530

    added

--- day4/p2.py
+++ day4/p2.py
@@ -6,6 +6,8 @@ print('Enter a number to find its Root')
 input_number = int(input()) #input takes always as a string
 
 root_number = math.sqrt(input_number)
-print('Sqaure root of ',input_number,'is',root_number)
-print('Sqaure root of ',input_number,'is '+ str(root_number))
+
+print('Square root of ',input_number,'is',root_number)
+print('Square root of ',input_number,'is '+ str(root_number))
+print(f'Sqaure root of {input_number} is {root_number}')
 
---
+++ day4/p4.py
@@ -0,0 +1,12 @@
+#program to print the numbers from m to n (m<n) with an increement of p
+
+m = int(input('Enter the M value: (start value): '))
+n = int(input('Enter the N value: (End value): '))
+p = int(input('Enter the P value: (Increeement value): '))
+
+i = m
+while i <= n:
+    print(i, end=' ')
+    i = i+p
+
+
